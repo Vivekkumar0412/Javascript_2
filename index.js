42c44ff40584493353223b04a819d9cc71bd21ef
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-06-07 23:16:30 +0530

    ES6 ECMA SCRIPT EXAMPLES AND DEFINATIONS ADDED

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Hi there");
+// console.log("Hi there");
+// ECMA Script 2015
+// ECMA Script or ES6 was a very important update in the history of javascript beacuse
+// it includes many features which were missing form the javascript.......after adding these features in javascript ,javascript become far more popular and useful. 
+
+// scope of var is functional
+// var name = "vivek";
+// console.log(name);
+// name = "kumar";
+// console.log(name);
+// so in the above code i can clearly see that value can be easily updated when we use var
+// let fname = "vivek";
+// console.log(fname);
+// fname = "kumar";
+// console.log(fname);
+// so in the above code i can clearly see that value can be easily updated when we use let also
+
+// const funame = "vivek";
+// console.log(funame);
+// funame = "kumar";
+// console.log(funame);
+// this time it will give an error because once we defined a value using const then we can not update it....its a fix value
+
+// function intro(){
+//     var fname = "Divya";
+//     if(true){
+//         var lname = "Singh"
+//         console.log("inner" + fname + lname);
+//     };
+//     console.log("inner_outer" + fname + lname);;
+// };
+// intro();
+
+// so here we saw that when we use var we were able to acess the values everywhere
+
+// function intro(){
+//     let fname = "Divya";
+//     if(true){
+//         let lname = "Singh"
+//         console.log("inner" + fname + lname);
+//     };
+//     console.log("inner_outer" + fname + lname);;
+// };
+// intro();
+
+// when we use let keyword we were not able to excess the lnme outside the if block because the lname was defined inside the if block...........and let scope is block
