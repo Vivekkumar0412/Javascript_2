5e5a21644c790583740f2b61d0275e41c9150ab4
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-06-10 23:07:43 +0530

    window.location and window .location.href defination and exaplme

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // console.log("Hi there");
 // ECMA Script 2015
 // ECMA Script or ES6 was a very important update in the history of javascript beacuse
-// it includes many features which were missing form the javascript.......after adding these features in javascript ,javascript become far more popular and useful. 
+// it includes many features which were missing form the javascript.......after adding these features in javascript ,javascript become far more popular and useful.
 
 // scope of var is functional
 // var name = "vivek";
@@ -45,10 +45,9 @@
 
 // when we use let keyword we were not able to excess the lnme outside the if block because the lname was defined inside the if block...........and let scope is block
 
-
 // string tamplet literals was alss added in the update of ES6 nd it is one of the most important update
 
-// when string tamplet litrels was not availabe it was very lengthy to consanate two strings and insert values inside it 
+// when string tamplet litrels was not availabe it was very lengthy to consanate two strings and insert values inside it
 
 // function table(a){
 //     for(let i = 1; i<=10; i++){
@@ -66,14 +65,12 @@
 
 // the output of the above code will be Nan
 
-
 // function mult(a,b=4){
 //     return a*b;
 // };
 // console.log(mult(2));
 
 // but here output is 8 because the default value of b is 4.
-
 
 // Fat Arrow functions was also added during the update of ES6
 
@@ -82,7 +79,7 @@
 // function sumi(){
 //     let a = 3;
 //     let b = 7;
-//     return `total sum is ${a+b}`; 
+//     return `total sum is ${a+b}`;
 // }
 
 // but fat arrow function will give error if the function is called first and then inilized
@@ -93,13 +90,13 @@
 // };
 // console.log(sum());
 
-// Arrays in javascript 
+// Arrays in javascript
 //  Arrays gives us the power to store multiple data a single point
 // in javascrpt we can store any type of value in array unlike other programming languages we can only store a single type of data in an array
 
 // Inilitizing of an array
 
-let name = ["Vivek","Divya","Pranav"];
+let name = ["Vivek", "Divya", "Pranav"];
 
 // Traversal of an array
 // excessing a single element
@@ -109,7 +106,6 @@ console.log(name[name.length - 1]);
 
 // .length is a property of an array that gives the length of an array, the value of length starts from 1
 
-
 // exatrating all the values of array using for loop
 // let num = [1,2,3,4,5,6,6,77,8788];
 // for(let i = 0; i<num.length; i++){
@@ -117,39 +113,39 @@ console.log(name[name.length - 1]);
 // };
 
 // FOR IN LOOP
-// for in loop basically return the index numbes of the elements present in an array 
-for(let elem in name){
-    console.log(elem)
-};
+// for in loop basically return the index numbes of the elements present in an array
+for (let elem in name) {
+  console.log(elem);
+}
 
 // FOR OF LOOP
 // for of loop return the original element present in the array
 
-for(h of name){
-    console.log(h);
-};
+for (h of name) {
+  console.log(h);
+}
 // forEach loop
 // forEach loop is very important loop in javascript.....until now we have seen that using for in loop we can get the index number of an array.......using for of loops we can get the element of an array.........but with the help of forEach loop we can get the element, index as well as the complete in a single loop.
-name.forEach(function(elem,ind,arr){
-    console.log(`${elem} index is : ${ind} and the array is ${arr}`)
+name.forEach(function (elem, ind, arr) {
+  console.log(`${elem} index is : ${ind} and the array is ${arr}`);
 });
 
 // SEARCHING AND FILTER METHOD IN ARRAY
 // indexOf() method.....it basically return the index of the elemnt if it is oresnt in the arry else it will retur -1.
 
-let naam = ["vivek", "pranav","sarthak"];
+let naam = ["vivek", "pranav", "sarthak"];
 console.log(naam.indexOf("vivek"));
 
 // by default the searching starts drom 0 but we can also define that from where the searching should start.
 
-console.log(naam.indexOf("sarthak",1));
+console.log(naam.indexOf("sarthak", 1));
 
 // 2nd Method of searching and filter in an array
 // lastIndexOf() it is same as the indexOf() method but the basic difference is it start its iterations from the last index and move from backward to farword.
 
 console.log(naam.lastIndexOf("sarthak"));
-// i can also define from where to start searching.   
-console.log(naam.lastIndexOf("sarthak",1)); //here it will result -1
+// i can also define from where to start searching.
+console.log(naam.lastIndexOf("sarthak", 1)); //here it will result -1
 
 // Third searching method i.e includes().....it basically returns true or false.if the value is present in the value it will return true if data is not present in the value it is false
 
@@ -188,18 +184,18 @@ let cout = 0;
 // const newprice = price.filter((elem,index)=>{
 //     return elem>300;
 // });
-// console.log(newprice); 
+// console.log(newprice);
 
 // sort() method is a method in array which is used to sort the array.
 // by default it sort the array in acending order
 // first it convert all the data into string and then sort it.
 
-let arr = [33,2,344,2,22,,443,22,498894,];
+let arr = [33, 2, 344, 2, 22, , 443, 22, 498894];
 console.log(arr.sort());
 
 // Insert delete CRUD method in an array
 // .push() method add the value at the last of array
-let arry = ["vivek","Lion","lala"];
+let arry = ["vivek", "Lion", "lala"];
 arry.push("pranav");
 console.log(arry);
 
@@ -221,41 +217,45 @@ console.log(arry);
 // splice() method in array
 // splice() method gives us the power to perform CRUD operation in a single go.
 
-let mr = ["hi","bye","jye"];
+let mr = ["hi", "bye", "jye"];
 // mr.splice(3,0,"tye_tye");
 // console.log(mr);
 
 // Splice() method that we will use in our genral coding
-mr.splice(1,1,"hu_hu");
+mr.splice(1, 1, "hu_hu");
 console.log(mr);
-let month = ["feb","march","april"]
+let month = ["feb", "march", "april"];
 let newmonth = month.indexOf("april");
-if(newmonth != -1){
-    month.splice(newmonth,1,"April");
-    console.log(month);
-}else{
-    console.log("No data found");
-};
+if (newmonth != -1) {
+  month.splice(newmonth, 1, "April");
+  console.log(month);
+} else {
+  console.log("No data found");
+}
 
 // forEach loop to sort the array
-let mri = [1,2,4,7,99,7,4,35,342,2,33,42,4,424,55,64,64,7,75,75];
+let mri = [
+  1, 2, 4, 7, 99, 7, 4, 35, 342, 2, 33, 42, 4, 424, 55, 64, 64, 7, 75, 75,
+];
 console.log(mri);
 let count = 0;
-mri.forEach(function(elem,index,array){
-    if(elem>9){
-        console.log(`the number is ${elem} whose index is ${index} and arrai is ${array}`);
-        count++;
-    }
+mri.forEach(function (elem, index, array) {
+  if (elem > 9) {
+    console.log(
+      `the number is ${elem} whose index is ${index} and arrai is ${array}`
+    );
+    count++;
+  }
 });
 console.log(`total numbers who are greater tha 9 is : ${count}`);
 
 // map() method in javascript
 // map() method returns a new array on the basis of operations we did on it
-let vi = [1,3,4,5,6,677,88999,786765,4];
+let vi = [1, 3, 4, 5, 6, 677, 88999, 786765, 4];
 let mount = 0;
-let newvi = vi.map((elem)=>{
-    console.log(elem>4);
-    mount++;
+let newvi = vi.map((elem) => {
+  console.log(elem > 4);
+  mount++;
 });
 console.log(`value of mount is ${mount}`);
 console.log(newvi);
@@ -264,102 +264,119 @@ console.log(newvi);
 
 // we can also excess each and every element but it will return in a form of array
 
-let newviv = vi.map((elem,index,array)=>{
-    console.log(`the index of ${elem} is ${index} it belond to ${array}`);
+let newviv = vi.map((elem, index, array) => {
+  console.log(`the index of ${elem} is ${index} it belond to ${array}`);
 });
 console.log(newviv);
 
 // Challenge question : - Find the sqaure root of every number in an array
-let ch1 = [1,2,3,4,5,6,7,8,9];
+let ch1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // let newch1 = ch1.map((elem,index,array)=>{
 //     return `the square of ${elem} will be ${elem*elem} whose index is ${index} and it belong to ${array}`;
 // });
 // console.log(newch1);
 // Challenge question : - Find the sqaure root of every number in an array using chaining in map() method
 
-let newch11 = ch1.map((elem)=>{
-    return elem = elem *2;
-}).filter((elem)=>{
-    return elem>10;
-});
+let newch11 = ch1
+  .map((elem) => {
+    return (elem = elem * 2);
+  })
+  .filter((elem) => {
+    return elem > 10;
+  });
 console.log(newch11);
 
 // i can write the above code in one line as well
 // here arrow function is returning only one line thats why we don,t need any return ketword.
-let newch112 = ch1.map((elem)=>{elem = elem *2;}).filter((elem)=>{elem>10;});
+let newch112 = ch1
+  .map((elem) => {
+    elem = elem * 2;
+  })
+  .filter((elem) => {
+    elem > 10;
+  });
 console.log(newch112);
 
 // This is a very important concept which states that we can use multilpe methods in map() method it will make our task eay
 
 // Challenge question 2 : - Multiply each element by 2 and return only that value who is greater than 10
 let sount = 0;
-let newch2 = ch1.map((elem)=>{
-    elem = elem*2;
-    if(elem>10){
-        console.log(`the elements are ${elem}`);
-        sount++; 
-    }
+let newch2 = ch1.map((elem) => {
+  elem = elem * 2;
+  if (elem > 10) {
+    console.log(`the elements are ${elem}`);
+    sount++;
+  }
 });
 console.log(newch2);
 console.log(sount);
 
-// Reduce() method is very useful method because it allow us to get a single value 
+// Reduce() method is very useful method because it allow us to get a single value
 //until now we were not capable to get results in one line but after reduce() method we are able to fetch the results in one line.
 
 // accumulator in the reduce() method is very important because it accyumulates all the data in a single varriable through which we can do multiple things like add, subyract, multipluy, didvide, average etc.
 
-let re = [1,2,3,4,5,6,7,8,9,7,7];
-let newre = re.reduce((accumulator,elem,index,array)=>{
-    return accumulator = accumulator + elem; 
+let re = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 7];
+let newre = re.reduce((accumulator, elem, index, array) => {
+  return (accumulator = accumulator + elem);
 });
 console.log(newre);
 
 // chanining
 
-let newre2 = re.map((elem)=>{
-    return elem = elem *2;
-}).filter((elem)=>{
-    return elem>10;
-}).reduce((accumulator,elem,index,array)=>{
-    return accumulator  = accumulator + elem;
-},6);
+let newre2 = re
+  .map((elem) => {
+    return (elem = elem * 2);
+  })
+  .filter((elem) => {
+    return elem > 10;
+  })
+  .reduce((accumulator, elem, index, array) => {
+    return (accumulator = accumulator + elem);
+  }, 6);
 
 console.log(newre2);
 
-
 // Reduce() method in one line
 
-let newre3 = re.map((elem)=> elem = elem*2).filter((elem)=> elem>10).reduce((accumulator,elem,index,array)=> accumulator = accumulator + elem);
+let newre3 = re
+  .map((elem) => (elem = elem * 2))
+  .filter((elem) => elem > 10)
+  .reduce(
+    (accumulator, elem, index, array) => (accumulator = accumulator + elem)
+  );
 console.log(newre3);
 
 // using inilitizing value in reduce()
-let newre21 = re.map((elem)=>{
-    return elem = elem *2;
-}).filter((elem)=>{
-    return elem>10;
-}).reduce((accumulator,elem,index,array)=>{
-    return accumulator  = accumulator + elem;
-},9);
+let newre21 = re
+  .map((elem) => {
+    return (elem = elem * 2);
+  })
+  .filter((elem) => {
+    return elem > 10;
+  })
+  .reduce((accumulator, elem, index, array) => {
+    return (accumulator = accumulator + elem);
+  }, 9);
 
 console.log(newre21);
 
 let flatarr = [
-    ['1','2',3],
-    ['4','5','6'],
-    ['7','8','9']
+  ["1", "2", 3],
+  ["4", "5", "6"],
+  ["7", "8", "9"],
 ];
 
-let newFlatarr = flatarr.reduce((accum,elem)=>{
-    return accum.concat(elem);
+let newFlatarr = flatarr.reduce((accum, elem) => {
+  return accum.concat(elem);
 });
-console.log(newFlatarr); 
-
+console.log(newFlatarr);
 
 // Strings in javascript
 // strings are basically used to add aur manipulate text
 // string can be written in single quote aur double quote
 
-let hu = 'hello world';
+let hu = "hello world";
 let hui = "Hello there";
 console.log(hu);
 console.log(hui);
@@ -368,10 +385,10 @@ console.log(hui);
 
 console.log(hu.length);
 
-// Escape characters 
-// we can use escape characters to print that value which will generally give error 
+// Escape characters
+// we can use escape characters to print that value which will generally give error
 
-let sentence = "hi there i am \"vivek kuamr\"";
+let sentence = 'hi there i am "vivek kuamr"';
 console.log(sentence);
 
 // we can also use alternate questes in order to escape from this situation
@@ -384,7 +401,7 @@ let myd = "Hello there my there name is vivek kumar";
 console.log(myd.indexOf("vivek")); //23
 
 // I can also decide that from where the searching should start
-console.log(myd.indexOf("there",7));
+console.log(myd.indexOf("there", 7));
 
 // lastIndexof() it basically start checking from the backword side of the string
 
@@ -403,11 +420,11 @@ console.log(myd.search("name"));
 // starting point is included but the ending ponit is not included
 
 let str = "Mango,Apple,Grapes";
-let newstr = str.slice(0,4);
+let newstr = str.slice(0, 4);
 console.log(newstr); //Mang
 // our output is man but it should have mango...whay that didnt happen because splice() method doesnt include last condition.
 
-let newstr2 = str.slice(7,-2);
+let newstr2 = str.slice(7, -2);
 // it will remove the last two element and show result between 7 and last
 let newstr3 = str.slice(7);
 // it will show result till last starting from 7
@@ -416,25 +433,26 @@ console.log(newstr3);
 
 // an example to how to show only limited text
 
-let tweet = " HI there i am vivek kumar this is just an example to how to show only limited text";
-console.log(tweet.slice(0,10));
+let tweet =
+  " HI there i am vivek kumar this is just an example to how to show only limited text";
+console.log(tweet.slice(0, 10));
 
-// Second method for is substring() method 
+// Second method for is substring() method
 // it is same as slice() method but it does not take negative numbers as argument
 // if i give a negative number in argument then it will start printing number form 0 until the given number excluding the last number
 
 let substr = "Apple, grapes, mango";
 console.log(substr.substring(10));
 // print from 10 to last
-console.log(substr.substring(8,-2));
+console.log(substr.substring(8, -2));
 // print from 0 to 8 but exclude the 8th position value
-console.log(substr.substring(7,9));
+console.log(substr.substring(7, 9));
 // print from 7 to 9
 
 // Third method
 // substr() method it is similar to slice() method but the basic differnce is that it takes length as second input
 
-console.log(substr.substr(0,5)); // Apple because it starts from 0 and print upto 5 excluding the value of 5.
+console.log(substr.substr(0, 5)); // Apple because it starts from 0 and print upto 5 excluding the value of 5.
 
 //  if i want to print last 4 characters only
 console.log(substr.substr(-4));
@@ -446,13 +464,13 @@ console.log(substr.substr(-4));
 // by default the it will only change the data which will be first
 
 let mname = " hi there my name is vivek kumar";
-console.log(mname.replace("kumar","singh"));
+console.log(mname.replace("kumar", "singh"));
 // console.log(mname.replace("","hi"));
 
 // Character exactracting methods
 // charAt() method
 // this method basically returns the character at the specific index number
-let hih = 'vivek kumar';
+let hih = "vivek kumar";
 console.log(hih.charAt(0)); // v
 
 // charCodeAt() is very important method
@@ -460,10 +478,9 @@ console.log(hih.charAt(0)); // v
 console.log(hih.charCodeAt(0)); //118
 // 118 is the character code of v in utf16
 
-
 // bonus question : - return the character code of last character
 let hy = "vivek kumar";
-console.log(hy.charCodeAt(hy.length -1));
+console.log(hy.charCodeAt(hy.length - 1));
 
 // property excess method in javascript
 // this was an update in ES5 (2009) this was very useful upadate
@@ -487,18 +504,17 @@ console.log(tr.trim());
 
 // converting string into array
 let arstr = "a,b,c,f";
-let newarstr = arstr.split(",");// on the basis of comma
-let newarstr2 = arstr.split(" ");// on the basis of space
-let newarstr3 = arstr.split("|");// on the basis of or 
-newarstr.push('v');
+let newarstr = arstr.split(","); // on the basis of comma
+let newarstr2 = arstr.split(" "); // on the basis of space
+let newarstr3 = arstr.split("|"); // on the basis of or
+newarstr.push("v");
 console.log(newarstr);
 console.log(newarstr2);
 console.log(newarstr3);
 
-
 // how to convert an arry into string
 // we use toString() method to convert an array into string.
-let starr = [1,2,3,4,4];
+let starr = [1, 2, 3, 4, 4];
 console.log(starr.toString());
 
 // Date object
@@ -513,14 +529,14 @@ console.log(d.toString());
 // remember one thing the counting starts fro 0 in javascript.
 // thats why 3 = April here.
 
-let v = new Date(2022,3,4,13,15,0,0);
+let v = new Date(2022, 3, 4, 13, 15, 0, 0);
 console.log(v.toLocaleString());
 
 // If we do Date() only then the output will be completely correct
 // but if i wirte Date(2011) i will get a wrong output
 
 let c = new Date(2010); // jan 1 1970 00:00
-let b = new Date(2010,6); // 6th may 2010
+let b = new Date(2010, 6); // 6th may 2010
 console.log(b.toLocaleString());
 
 // remenber one thing that if we are giveing any argument in Date() then minimum we shoud give 2 argument.
@@ -572,7 +588,7 @@ console.log(at.toLocaleDateString());
 // if i want time only.
 console.log(at.toLocaleTimeString());
 
-// Math object 
+// Math object
 // return the value of PI
 console.log(Math.PI);
 // round off the number in normal convention
@@ -587,8 +603,8 @@ console.log(Math.sqrt(66));
 // returns the absplute value always convert the negative value to positive
 console.log(Math.abs(-99));
 
-// use to know the power of any number 
-console.log(Math.pow(2,3));
+// use to know the power of any number
+console.log(Math.pow(2, 3));
 
 // it chop off the number before decimal and it always return a postive number
 console.log(Math.floor(99.883));
@@ -600,13 +616,19 @@ console.log(Math.trunc(-99.77));
 console.log(Math.random());
 
 // generate number between 1 to 10.
- console.log(Math.floor(Math.random() * 11) );
+console.log(Math.floor(Math.random() * 11));
 
- // Window object in javascript
- // Everything that we see in the browser is window object
- // document is the child of window object
- // we can say that everyting that belong to document it also belong to window object but everything that belong to window object it is not necessary that it should also belong to document.
+// Window object in javascript
+// Everything that we see in the browser is window object
+// document is the child of window object
+// we can say that everyting that belong to document it also belong to window object but everything that belong to window object it is not necessary that it should also belong to document.
 
+// Window.location is used to find all the details of current web page
+console.log(window.location);
 
+// window.location.href is used to find the current url
+console.log(window.location.href);
 
-
+// even if we dont write window keyword in bith the codes then also it will give us same results.
+console.log(location.href);
+console.log(location.href);
